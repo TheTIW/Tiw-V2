70cad7f0440ba6cbffd1f75a22b16968810b9ec6
NCCoder1 <119767145+NCCoder1@users.noreply.github.com>
2023-06-06 22:48:55 +0000

    Stuff

--- games/script.js
+++ games/script.js
@@ -6,3 +6,32 @@ function goBack() {
     location.reload();
   }
   
+  function toggleFullscreen() {
+    const iframe = document.getElementById('iframe');
+    if (iframe.requestFullscreen) {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        iframe.requestFullscreen();
+      }
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+      if (document.mozFullScreenElement) {
+        document.mozCancelFullScreen();
+      } else {
+        iframe.mozRequestFullScreen();
+      }
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      if (document.webkitFullscreenElement) {
+        document.webkitExitFullscreen();
+      } else {
+        iframe.webkitRequestFullscreen();
+      }
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+      if (document.msFullscreenElement) {
+        document.msExitFullscreen();
+      } else {
+        iframe.msRequestFullscreen();
+      }
+    }
+  }
+  
